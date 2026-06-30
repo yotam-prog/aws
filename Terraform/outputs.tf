aafa8d7f7d6dced5e9633b54df4c2a3006398573
Yotam Aloni <34979474+yotam-prog@users.noreply.github.com>
2026-06-30 16:11:24 +0300

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,3 +3,15 @@ output "instance_hostname" {
   value       = aws_instance.app_server.private_dns
 }
 
+output "instance_security_group_ids" {
+  value = aws_instance.app_server.vpc_security_group_ids
+}
+
+output "instance_subnet" {
+  value = aws_instance.app_server.subnet_id
+}
+
++ output "instance_ami" {
++   description = "AMI used to create the EC2 instance."
++   value       = aws_instance.app_server.ami
++ }
